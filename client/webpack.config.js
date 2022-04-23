6f0df2b242c5cdd69fa6238bc47ec3dcf5189389
jmaraya1229 <jessemaraya2013@gmail.com>
2022-04-23 16:16:11 -0700

    edit htmlwebpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,8 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-
+        template: './index.html',
+        title: 'JATE'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
